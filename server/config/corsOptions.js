5af04cfb82d409fe67a4cdd87fae37aa79f528d2
Thang Za Thang <Thangzathang@gmail.com>
2023-02-09 11:29:30 +1100

    cors option moved to it own folder

---
+++ server/config/corsOptions.js
@@ -0,0 +1,24 @@
+// Cors whitelist
+const whitelist = [
+  //
+  "http://localhost:3000",
+  process.env.FRONT_END_URL,
+];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    /* 
+    -1 means does not exist, but here we are checking !== so in context 
+     we are making sure the domain exists in the whitelist 
+     */
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Domain not in the whitelist. Not allowed by CORS origin."));
+    }
+  },
+  optionSuccessStatus: 200,
+  credentials: true,
+};
+
+module.exports = corsOptions;
--- server/index.js
+++ server/index.js
@@ -7,72 +7,35 @@ const path = require("path");
 
 // Imports
 const errorHandler = require("./middleware/errorHandler");
+const corsOptions = require("./config/corsOptions");
 
-// Database
-const pool = require("./db");
+// Import auth routes
+const authRoutes = require("./routes/authRoutes");
 
+/* Init App */
 const app = express();
 
-// Cors whitelist
-const whitelist = [
-  //
-  "http://localhost:3000",
-  process.env.FRONT_END_URL,
-];
-
-const corsOptions = {
-  origin: (origin, callback) => {
-    /* 
-    -1 means does not exist, but here we are checking !== so in context 
-     we aare makign sure the domain exists in the whitelist 
-     */
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error("Domain not in the whitelist. Not allowed by CORS origin."));
-    }
-  },
-  optionSuccessStatus: 200,
-  credentials: true,
-};
-// console.log("Cors Option:", corsOptions);
-
-// Middleware
+/* Middleware */
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 
-// Heroku configs
+// PORT config
 const PORT = process.env.PORT || 5000;
 
+// To show something to the user
 app.get("/", (req, res) => {
   res.send("This is the P.E.R.N stack application!");
 });
 
+// Heroku production app
 if (process.env.NODE_ENV === "production") {
   // serve static content
   app.use(express.static(path.join(__dirname, "client/build")));
 }
 
-// Import auth routes
-const authRoutes = require("./routes/authRoutes");
-// const homepageRoutes = require("./routes/homepage");
-// const userRoutes = require("./routes/userRoutes");
-
-// console.log("PostgresSQL Connection Status:", pool);
-
-// Register and Login Routes.
+/* Routes */
 app.use("/auth", authRoutes);
-
-// Homepage
-// app.use("/homepage", homepageRoutes);
-
-// User
-// app.use("/user", userRoutes);
-
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "client/build/index.html"));
-// });
 
 /* Error catcher */
 app.all("*", (req, res) => {
