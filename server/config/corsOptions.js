505cf3a8255bf8ca14f478856f8376fe64882f00
Thang Za Thang <Thangzathang@gmail.com>
2023-02-10 16:13:06 +1100

    whitelist changed to allowed origin

--- server/config/corsOptions.js
+++ server/config/corsOptions.js
@@ -1,5 +1,5 @@
 // Cors whitelist
-const whitelist = [
+const allowedOrigins = [
   //
   "http://localhost:3000",
   process.env.FRONT_END_URL,
@@ -11,7 +11,7 @@ const corsOptions = {
     -1 means does not exist, but here we are checking !== so in context 
      we are making sure the domain exists in the whitelist 
      */
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
       callback(null, true);
     } else {
       callback(new Error("Domain not in the whitelist. Not allowed by CORS origin."));
