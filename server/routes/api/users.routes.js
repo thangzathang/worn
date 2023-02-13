648d34a8d665845616aec0d2b089acf0e707e80b
Thang Za Thang <Thangzathang@gmail.com>
2023-02-13 18:30:03 +1100

    user roles based auth added for getting users

--- server/routes/api/users.routes.js
+++ server/routes/api/users.routes.js
@@ -1,16 +1,15 @@
 const router = require("express").Router();
-const verifyJWT = require("../../middleware/verifyJWT");
+
+const ROLES_LIST = require("../../config/roles_lists");
+
+/* Middleware */
+const verifyRoles = require("../../middleware/verifyRoles");
 
 // Get controller
 const userController = require("../../controllers/users.controller");
 
 // If we want to protect all /user routes there is a better way.
 // Checkout index file
-router.get(
-  "/",
-  //
-  //   verifyJWT,
-  userController.getAllUsers
-);
+router.get("/", verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor), userController.getAllUsers);
 
 module.exports = router;
