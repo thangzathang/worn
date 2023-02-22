5f923e480f0bb9350bf0c5d945b71f4997aa3e73
Thang Za Thang <Thangzathang@gmail.com>
2023-02-22 12:47:09 +1100

    Auth provider and basic route added

--- client/src/index.js
+++ client/src/index.js
@@ -3,9 +3,21 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
+// Context
+import { AuthProvider } from "./context/AuthProvider";
+
+// React Router
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <AuthProvider>
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
