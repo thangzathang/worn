10d76d7a9942e82beb563259af5439d2f8a676ed
Thang Za Thang <Thangzathang@gmail.com>
2023-02-22 12:53:47 +1100

    global auth hook added

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,61 @@
+// All Pages
 import Register from "./Register";
 import Login from "./Login";
+import Admin from "./components/Admin";
+import Home from "./components/Home";
+import LinkPage from "./components/LinkPage";
+import Lounge from "./components/Lounge";
+import Layout from "./components/Layout";
+import Editor from "./components/Editor";
+import Unauthorized from "./components/Unauthorized";
+import Missing from "./components/Missing";
 
-// Context
-import { AuthProvider } from "./context/AuthProvider";
+// react-router-dom
+import RequireAuth from "./components/RequireAuth";
+import { Routes, Route } from "react-router-dom";
+
+// Defined Roles
+const ROLES = {
+  User: 2001,
+  Editor: 1984,
+  Admin: 5150,
+};
 
 function App() {
   return (
-    <main className="App">
-      <AuthProvider>
-        <Login />
-      </AuthProvider>
-    </main>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        {/* Public routes */}
+
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+        <Route path="linkpage" element={<LinkPage />} />
+        <Route path="unauthorized" element={<Unauthorized />} />
+
+        <Route path="/" element={<Home />} />
+        <Route path="editor" element={<Editor />} />
+
+        {/* Protected */}
+        {/* <Route element={<RequireAuth allowedRoles={[ROLES.User]} />}>
+          <Route path="/" element={<Home />} />
+        </Route>
+
+        <Route element={<RequireAuth allowedRoles={[ROLES.Editor]} />}>
+          <Route path="editor" element={<Editor />} />
+        </Route>
+
+        <Route element={<RequireAuth allowedRoles={[ROLES.Admin]} />}>
+          <Route path="admin" element={<Admin />} />
+        </Route>
+
+        <Route element={<RequireAuth allowedRoles={[ROLES.Editor, ROLES.Admin]} />}>
+          <Route path="lounge" element={<Lounge />} />
+        </Route> */}
+
+        {/* catch all */}
+        <Route path="*" element={<Missing />} />
+      </Route>
+    </Routes>
   );
 }
 
